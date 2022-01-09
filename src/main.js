092c233c7d3b65d9410fcd4eeffd4ea5c2ee666b
Michael Fu <49467589+michaelyfu@users.noreply.github.com>
2022-01-08 20:20:25 -0500

    basic save persisting functionality

--- src/main.js
+++ src/main.js
@@ -5,76 +5,21 @@ var notesContainer = document.getElementById("notes");
 printLocalStorage();
 
 function printLocalStorage() {
-    // const note = document.createElement("textarea");
-    // notesContainer.innerHTML += localStorage.getItem("localStorageNotes");
-    // getNotes().forEach((note) => {
-    //     // const printedNote = document.createElement("textarea");
-    //     // // printedNote.setAttribute("id", "content");
-    //     // // printedNote.classList.add("note");
-    //     // // printedNote.id = note.id;
-    //     // printedNote.value = note.content;
-
-    //     notesContainer.appendChild(note);
-
-
-    const allItems = JSON.parse(localStorage.getItem("localStorageNotes"));
-    console.log(allItems);
-    // allItems.forEach(note => document.body.appendChild(note));
+    getNotes().forEach(note => {
+        const noteElement = create(note.id, note.content, note.x, note.y);
+        notesContainer.appendChild(noteElement);
+    });
 }
-//     for(let i = 0; i < getNotes().length; i++) {
-//         const key = localStorage.key(i);
-//         const value = localStorage.getItem(key);
-
-//         const note = document.createElement("textarea");
-//         note.setAttribute("id", "content");
-//         note.classList.add("note");
-//         note.content = value;
-
-//         notesContainer.appendChild(note);
-//     }
-// }
 
 function getNotes() {
     return JSON.parse(localStorage.getItem("localStorageNotes") || "[]");
-}
+  }
 
-function saveNotes(notes) {
-    localStorage.setItem("localStorageNotes", JSON.stringify(notes)) 
-}
-
-// function newNote() {
-//     const allNotes = getNotes();
-
-//     const emptyNote = {
-//         id: Math.random() * 1000,
-//         content: ""
-//     };
-
-//     const note = document.createElement("textarea");
-//     note.setAttribute("id", "content");
-//     note.classList.add("note");
-//     note.id = emptyNote.id;
-//     note.content = emptyNote.content;
-
-//     const notesContainer = document.getElementById("notes");
-//     notesContainer.appendChild(note); //adds element to website, doesn't save and not permanent
-
-//     allNotes.push(emptyNote);
-//     saveNotes(allNotes); //saves notes to localStorage
-
-//     note.addEventListener("change", () => {
-//         updateNote(note.id, note.value);
-//     });
-
-//     note.addEventListener("click", () => {
-//         deleteNote(note.id, note);
-//     });
-    
-//     // console.log(typeof emptyNote);
-// }
+  function saveNotes(notes) {
+    localStorage.setItem("localStorageNotes", JSON.stringify(notes));
+  }
 
 function updateNote(id, updateContent) {
-
     const allNotes = getNotes();
     const updatingNote = allNotes.filter((note) => note.id == id)[0];
     updatingNote.content = updateContent;
@@ -89,34 +34,71 @@ function deleteNote(id, element) {
     
 }
 
+function addNote(event) {
+    console.log("addNote is being called");
 
-function create(event) {
-    //Create the image
-    var i = document.createElement("textarea");
-  
     const allNotes = getNotes();
 
     const emptyNote = {
         id: Math.random() * 1000,
-        content: ""
+        content: "",
+        x: 0,
+        y: 0,
     };
 
+    emptyNote.x = event.clientX;
+    emptyNote.y = event.clientY;
+    console.log("hi");
+
+
+    const newNote = create(emptyNote.id, emptyNote.content, emptyNote.x, emptyNote.y, 'dblclick'); // not passing in an event rn
+    notesContainer.appendChild(newNote);
+
+    allNotes.push(emptyNote);
+    saveNotes(allNotes);
+}
+
+function create(id, content, x, y) {
+    //Create the image
+    const i = document.createElement("textarea");
+
     //Set the source of the image
-    i.setAttribute("id", "content");
+    i.setAttribute("id", "content", "x", "y");
     i.classList.add("note");
-    i.id = emptyNote.id;
-    i.content = emptyNote.content;
+    i.id = id;
+    i.content = content;
+    i.value = content;
+    console.log(i.content);
+    i.x = x;
+    i.y = y;
 
     // const notesContainer = document.getElementById("notes");
     // notesContainer.appendChild(note); //adds element to website, doesn't save and not permanent
 
-    allNotes.push(emptyNote);
-    saveNotes(allNotes); //saves notes to localStorage
-  
+    // allNotes.push(emptyNote);
+    // saveNotes(allNotes); //saves notes to localStorage
+
+
+    // if we're creating a new element
+    // if(i.x == 0) {
+    //     i.style.left = event.clientX + 'px';
+    //     i.style.top = event.clientY + 'px';
+    //     i.x = event.clientX;
+    //     i.y = event.clientY;      
+    // }
+
+    // // if reloading the page
+    // else {
+    //     i.style.left = i.x + 'px';
+    //     i.y = i.y + 'px';
+    // }
+
     //Set CSS styles so it appears where you clicked (Top left corner)
     i.style.position = 'absolute';
-    i.style.left     = event.clientX + 'px';
-    i.style.top      = event.clientY + 'px';
+    i.style.left     = i.x + 'px';
+    i.style.top      = i.y + 'px';
+    // i.x = event.clientX;
+    // i.y = event.clientY;
 
     i.addEventListener("change", () => {
         updateNote(i.id, i.value);
@@ -125,9 +107,13 @@ function create(event) {
     i.addEventListener("click", () => {
         deleteNote(i.id, i);
     });
+
     
     //Add it to the body of the document
-    notesContainer.appendChild(i);
+    // notesContainer.appendChild(i);
+
+    return i;
   }
   //Main event listener for clicks
-  document.addEventListener('dblclick', create);
+  document.addEventListener('keypress', addNote);
+
